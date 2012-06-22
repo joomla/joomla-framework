47d6f33da126f425aa8630f3bb6f2a072596b182
dianaprajescu <diana.prajescu@gmail.com>
2012-06-22 19:20:46 +0300

    Start writing the oauth 1.0a client for Twitter.
    Twitter authentication draft.
    
    Working OAuth.
    
    Add POST request to sendRequest() method. Posting tweets works :-).
    
    Add getOption() and setOption() methods to JTwitterObject. Change JTwitter 'api.url' option.
    
    Add tweet() method parameters. OAuth improvements.
    
    Add code in oauthRequest to validate the response. Change the error string in JTwitterStatusesTest.
    
    Twitter uses only OAuth, remove code for basic authentication in fetchUrl(). Remove unused constants in JTwitterHttp.
    
    Remove sendRequest() $code parameter, it is not needed. 200 is the only code for a successful request, anything else is an error.
    
    Tests for tweet() method.
    
    Add setToken() method to JTwitterOAuth.
    
    Remove $code from sendRequest method.

--- tests/suites/unit/joomla/twitter/JTwitterStatusesTest.php
+++ tests/suites/unit/joomla/twitter/JTwitterStatusesTest.php
@@ -10,10 +10,16 @@
 require_once JPATH_PLATFORM . '/joomla/twitter/twitter.php';
 require_once JPATH_PLATFORM . '/joomla/twitter/http.php';
 require_once JPATH_PLATFORM . '/joomla/twitter/statuses.php';
+require_once JPATH_PLATFORM . '/joomla/twitter/oauth.php';
 
 /**
- * Test class for JTwitterStatuses.
- */
+* Test class for JTwitterStatuses.
+*
+* @package     Joomla.UnitTest
+* @subpackage  Twitter
+*
+* @since       12.1
+*/
 class JTwitterStatusesTest extends TestCase
 {
 	/**
@@ -35,29 +41,49 @@ class JTwitterStatusesTest extends TestCase
 	protected $object;
 
 	/**
+	 * @var    JTwitterOAuth  Authentication object for the Twitter object.
+	 * @since  12.1
+	 */
+	protected $oauth;
+
+	/**
 	 * @var    string  Sample JSON string.
 	 * @since  12.1
 	 */
 	protected $sampleString = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
 
 	/**
+	 * @var    string  Sample JSON string.
+	 * @since  12.1
+	 */
+	protected $rateLimit = '{"remaining_hits":150, "reset_time":"Mon Jun 25 17:20:53 +0000 2012"}';
+
+	/**
 	 * @var    string  Sample JSON error message.
 	 * @since  12.1
 	 */
-	protected $errorString = '{"message": "Generic Error"}';
+	protected $errorString = '{"error": "Generic Error."}';
 
 	/**
 	 * Sets up the fixture, for example, opens a network connection.
 	 * This method is called before a test is executed.
 	 *
 	 * @access protected
+	 * 
+	 * @return void
 	 */
 	protected function setUp()
 	{
+		$key = "lIio7RcLe5IASG5jpnZrA";
+		$secret = "dl3BrWij7LT04NUpy37BRJxGXpWgjNvMrneuQ11EveE";
+		$my_url = "http://127.0.0.1/gsoc/joomla-platform/twitter_test.php";
+
 		$this->options = new JRegistry;
 		$this->client = $this->getMock('JTwitterHttp', array('get', 'post', 'delete', 'put'));
 
 		$this->object = new JTwitterStatuses($this->options, $this->client);
+		$this->oauth = new JTwitterOAuth($key, $secret, $my_url, $this->client);
+		$this->oauth->setToken($key, $secret);
 	}
 
 	protected function getMethod($name)
@@ -103,9 +129,6 @@ class JTwitterStatusesTest extends TestCase
 	 */
 	public function testGetRetweetedByUserFailure()
 	{
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete('This test currently fails.');
-
 		$returnData = new stdClass;
 		$returnData->code = 500;
 		$returnData->body = $this->errorString;
@@ -153,9 +176,6 @@ class JTwitterStatusesTest extends TestCase
 	 */
 	public function testGetTweetByIdFailure()
 	{
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete('This test currently fails.');
-
 		$returnData = new stdClass;
 		$returnData->code = 500;
 		$returnData->body = $this->errorString;
@@ -178,13 +198,18 @@ class JTwitterStatusesTest extends TestCase
 	public function testGetUserTimeline()
 	{
 		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete('This test currently fails due to checkRateLimit.');
+		$this->markTestIncomplete('This test currently fails.');
 
 		$returnData = new stdClass;
 		$returnData->code = 200;
 		$returnData->body = $this->sampleString;
 
-		$this->client->expects($this->at(2))
+		/*$this->client->expects($this->at(0))
+		->method('get')
+		->with('/1/account/rate_limit_status.json')
+		->will($this->returnValue($returnData));*/
+
+		$this->client->expects($this->at(1))
 		->method('get')
 		->with('/1/statuses/user_timeline.json?screen_name=joomla&count=20&include_rts=true')
 		->will($this->returnValue($returnData));
@@ -219,5 +244,78 @@ class JTwitterStatusesTest extends TestCase
 		->will($this->returnValue($returnData));
 
 		$this->object->getUserTimeline('joomla');
+	}
+
+	/**
+	 * Tests the tweet method
+	 *
+	 * @return  void
+	 *
+	 * @since   12.1
+	 */
+	public function testTweet()
+	{
+		$status = 'This is a status';
+		$in_reply_to_status_id = 1336421235;
+		$lat = 42.53;
+		$long = 45.21;
+		$place_id = '23455ER235V';
+		$display_coordinates = true;
+		$trim_user = true;
+		$entities = true;
+
+		$returnData = new stdClass;
+		$returnData->code = 200;
+		$returnData->body = $this->sampleString;
+
+		// Set POST request parameters.
+		$data = array();
+		$data['status'] = utf8_encode($status);
+		$data['in_reply_to_status_id'] = $in_reply_to_status_id;
+		$data['lat'] = $lat;
+		$data['long'] = $long;
+		$data['place_id'] = $place_id;
+		$data['display_coordinates'] = $display_coordinates;
+		$data['trim_user'] = $trim_user;
+		$data['include_entities'] = $entities;
+
+		$this->client->expects($this->once())
+			->method('post')
+			->with('/1/statuses/update.json', $data)
+			->will($this->returnValue($returnData));
+
+		$this->assertThat(
+		$this->object->tweet($this->oauth, $status, $in_reply_to_status_id, $lat, $long, $place_id, $display_coordinates, $trim_user, $entities),
+		$this->equalTo(json_decode($this->sampleString))
+		);
+	}
+
+	/**
+	 * Tests the tweet method
+	 *
+	 * @return  void
+	 *
+	 * @since   12.1
+	 *
+	 * @expectedException  DomainException
+	 */
+	public function testTweetFailure()
+	{
+		$status = 'This is a status';
+
+		$returnData = new stdClass;
+		$returnData->code = 500;
+		$returnData->body = $this->errorString;
+
+		// Set POST request parameters.
+		$data = array();
+		$data['status'] = utf8_encode($status);
+
+		$this->client->expects($this->once())
+			->method('post')
+			->with('/1/statuses/update.json', $data)
+			->will($this->returnValue($returnData));
+
+		$this->object->tweet($this->oauth, $status);
 	}
 }
