685e8d0648f739b88c841bab60346f8ce11f76be
dianaprajescu <diana.prajescu@gmail.com>
2012-07-05 17:01:37 +0300

    Improve the Help object.
    Add getConfiguration() method to JTwitterHelp.
    
    Add test() method to JTwitterHelp.

--- tests/suites/unit/joomla/twitter/JTwitterHelpTest.php
+++ tests/suites/unit/joomla/twitter/JTwitterHelpTest.php
@@ -52,6 +52,12 @@ class JTwitterHelpTest extends TestCase
 	protected $errorString = '{"errors":[{"message":"Sorry, that page does not exist","code":34}]}';
 
 	/**
+	 * @var    string  Sample JSON string.
+	 * @since  12.1
+	 */
+	protected $rateLimit = '{"remaining_hits":150, "reset_time":"Mon Jun 25 17:20:53 +0000 2012"}';
+
+	/**
 	 * Sets up the fixture, for example, opens a network connection.
 	 * This method is called before a test is executed.
 	 *
@@ -59,19 +65,201 @@ class JTwitterHelpTest extends TestCase
 	 */
 	protected function setUp()
 	{
+		$key = "lIio7RcLe5IASG5jpnZrA";
+		$secret = "dl3BrWij7LT04NUpy37BRJxGXpWgjNvMrneuQ11EveE";
+		$my_url = "http://127.0.0.1/gsoc/joomla-platform/twitter_test.php";
+
 		$this->options = new JRegistry;
 		$this->client = $this->getMock('JTwitterHttp', array('get', 'post', 'delete', 'put'));
 
 		$this->object = new JTwitterHelp($this->options, $this->client);
+		$this->oauth = new JTwitterOAuth($key, $secret, $my_url, $this->client);
+		$this->oauth->setToken($key, $secret);
 	}
 
 	/**
-	 * @covers JTwitterHelp::getLanguages
-	 * @todo   Implement testGetLanguages().
+	 * Tests the getLanguages method
+	 *
+	 * @return  void
+	 *
+	 * @since   12.1
 	 */
 	public function testGetLanguages()
 	{
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		$returnData = new stdClass;
+		$returnData->code = 200;
+		$returnData->body = $this->rateLimit;
+
+		$this->client->expects($this->at(0))
+		->method('get')
+		->with('/1/account/rate_limit_status.json')
+		->will($this->returnValue($returnData));
+
+		$returnData = new stdClass;
+		$returnData->code = 200;
+		$returnData->body = $this->sampleString;
+
+		$path = $this->object->fetchUrl('/1/help/languages.json');
+
+		$this->client->expects($this->at(1))
+		->method('get')
+		->with($path)
+		->will($this->returnValue($returnData));
+
+		$this->assertThat(
+			$this->object->getLanguages(),
+			$this->equalTo(json_decode($this->sampleString))
+		);
+	}
+
+	/**
+	 * Tests the getLanguages method - failure
+	 *
+	 * @return  void
+	 *
+	 * @since   12.1
+	 * @expectedException DomainException
+	 */
+	public function testGetLanguagesFailure()
+	{
+		$returnData = new stdClass;
+		$returnData->code = 200;
+		$returnData->body = $this->rateLimit;
+
+		$this->client->expects($this->at(0))
+		->method('get')
+		->with('/1/account/rate_limit_status.json')
+		->will($this->returnValue($returnData));
+
+		$returnData = new stdClass;
+		$returnData->code = 500;
+		$returnData->body = $this->errorString;
+
+		$path = $this->object->fetchUrl('/1/help/languages.json');
+
+		$this->client->expects($this->at(1))
+		->method('get')
+		->with($path)
+		->will($this->returnValue($returnData));
+
+		$this->object->getLanguages();
+	}
+
+	/**
+	 * Tests the getConfiguration method
+	 *
+	 * @return  void
+	 *
+	 * @since   12.1
+	 */
+	public function testGetConfiguration()
+	{
+		$returnData = new stdClass;
+		$returnData->code = 200;
+		$returnData->body = $this->rateLimit;
+
+		$this->client->expects($this->at(0))
+		->method('get')
+		->with('/1/account/rate_limit_status.json')
+		->will($this->returnValue($returnData));
+
+		$returnData = new stdClass;
+		$returnData->code = 200;
+		$returnData->body = $this->sampleString;
+
+		$path = $this->object->fetchUrl('/1/help/configuration.json');
+
+		$this->client->expects($this->at(1))
+		->method('get')
+		->with($path)
+		->will($this->returnValue($returnData));
+
+		$this->assertThat(
+			$this->object->getConfiguration(),
+			$this->equalTo(json_decode($this->sampleString))
+		);
+	}
+
+	/**
+	 * Tests the getConfiguration method - failure
+	 *
+	 * @return  void
+	 *
+	 * @since   12.1
+	 * @expectedException DomainException
+	 */
+	public function testGetConfigurationFailure()
+	{
+		$returnData = new stdClass;
+		$returnData->code = 200;
+		$returnData->body = $this->rateLimit;
+
+		$this->client->expects($this->at(0))
+		->method('get')
+		->with('/1/account/rate_limit_status.json')
+		->will($this->returnValue($returnData));
+
+		$returnData = new stdClass;
+		$returnData->code = 500;
+		$returnData->body = $this->errorString;
+
+		$path = $this->object->fetchUrl('/1/help/configuration.json');
+
+		$this->client->expects($this->at(1))
+		->method('get')
+		->with($path)
+		->will($this->returnValue($returnData));
+
+		$this->object->getConfiguration();
+	}
+
+	/**
+	 * Tests the test method
+	 *
+	 * @return  void
+	 *
+	 * @since   12.1
+	 */
+	public function testTest()
+	{
+		$returnData = new stdClass;
+		$returnData->code = 200;
+		$returnData->body = $this->sampleString;
+
+		$path = $this->object->fetchUrl('/1/help/test.json');
+
+		$this->client->expects($this->once())
+		->method('get')
+		->with($path)
+		->will($this->returnValue($returnData));
+
+		$this->assertThat(
+			$this->object->test(),
+			$this->equalTo(json_decode($this->sampleString))
+		);
+	}
+
+	/**
+	 * Tests the test method - failure
+	 *
+	 * @return  void
+	 *
+	 * @since   12.1
+	 * @expectedException DomainException
+	 */
+	public function testTestFailure()
+	{
+		$returnData = new stdClass;
+		$returnData->code = 500;
+		$returnData->body = $this->errorString;
+
+		$path = $this->object->fetchUrl('/1/help/test.json');
+
+		$this->client->expects($this->once())
+		->method('get')
+		->with($path)
+		->will($this->returnValue($returnData));
+
+		$this->object->test();
 	}
 }
