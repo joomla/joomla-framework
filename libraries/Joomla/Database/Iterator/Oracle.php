d5575703be00519148b32df6a1666107350290ca
Michael Babker <mbabker@flbab.com>
2013-02-24 14:42:04 -0600

    Fix namespace

--- libraries/Joomla/Database/Iterator/Oracle.php
+++ libraries/Joomla/Database/Iterator/Oracle.php
@@ -7,7 +7,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
-namespace Joomla\Database\Itator;
+namespace Joomla\Database\Iterator;
 
 defined('JPATH_PLATFORM') or die;
 
