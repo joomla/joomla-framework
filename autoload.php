3924b34c65cdda012f1d1cdcf5f3d9db05b92c76
Don Gilbert <don@dongilbert.net>
2013-03-01 00:17:16 -0600

    Removing placeholder autoload. Fixing json parse error in registry/composer.json

--- autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @copyright   Copyright (C) 2005 - 2013 Open Source Matters. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-require_once __DIR__ . '/vendor/import.php';
