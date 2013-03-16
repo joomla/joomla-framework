319e696b8f9a09d0c861e29f0caf187a41fa55ac
Don Gilbert <don@dongilbert.net>
2013-03-16 00:22:12 -0500

    Code style fix. Remove trailing whitespace.

--- vendor/Joomla/Registry/Registry.php
+++ vendor/Joomla/Registry/Registry.php
@@ -404,7 +404,7 @@ class Registry implements \JsonSerializable, \ArrayAccess
 		if ($nodes)
 		{
 			// Initialize the current node to be the registry root.
-			$node = $this->data;			
+			$node = $this->data;
 
 			// Traverse the registry to find the correct node for the result.
 			for ($i = 0, $n = count($nodes) - 1; $i < $n; $i++)
