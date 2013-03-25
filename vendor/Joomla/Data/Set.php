16de0f531363ca86adce564ad516857eb2f98937
Don Gilbert <don@dongilbert.net>
2013-03-25 01:14:03 -0500

    Adding short descriptions for phpDoc [ci skip]

--- vendor/Joomla/Data/Set.php
+++ vendor/Joomla/Data/Set.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Part of the Joomla Framework Data Package
+ *
  * @copyright  Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
