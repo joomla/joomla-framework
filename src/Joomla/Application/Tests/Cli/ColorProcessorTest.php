f5e5807ee8e9380f5b99ac4eff9a9e0a808e6a95
Nikolai Plath <der.el.kuku@gmail.com>
2013-05-06 21:16:39 -0500

    Add colors to CLI applications

---
+++ src/Joomla/Application/Tests/Cli/ColorProcessorTest.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @copyright  Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Application\Tests;
+
+use Joomla\Application\Cli\ColorProcessor;
+use Joomla\Application\Cli\ColorStyle;
+
+/**
+ * Test class.
+ *
+ * @since  1.0
+ */
+class ColorProcessorTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @var ColorProcessor
+	 */
+	protected $object;
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 */
+	protected function setUp()
+	{
+		$this->object = new ColorProcessor;
+	}
+
+	/**
+	 * @covers Joomla\Application\Cli\ColorProcessor::addStyle
+	 */
+	public function testAddStyle()
+	{
+		$style = new ColorStyle('red');
+		$this->object->addStyle('foo', $style);
+
+		$this->assertThat(
+			$this->object->process('<foo>foo</foo>'),
+			$this->equalTo('[31mfobo[0m')
+		);
+	}
+
+	/**
+	 * @covers Joomla\Application\Cli\ColorProcessor::stripColors
+	 */
+	public function testStripColors()
+	{
+		$this->assertThat(
+			$this->object->stripColors('<foo>foo</foo>'),
+			$this->equalTo('foo')
+		);
+	}
+
+	/**
+	 * @covers Joomla\Application\Cli\ColorProcessor::process
+	 */
+	public function testProcess()
+	{
+		$this->assertThat(
+			$this->object->process('<fg=red>foo</fg=red>'),
+			$this->equalTo('[31mfoo[0m')
+		);
+	}
+
+	/**
+	 * @covers Joomla\Application\Cli\ColorProcessor::process
+	 */
+	public function testProcessNamed()
+	{
+		$style = new ColorStyle('red');
+		$this->object->addStyle('foo', $style);
+
+		$this->assertThat(
+			$this->object->process('<foo>foo</foo>'),
+			$this->equalTo('[31mfoo[0m')
+		);
+	}
+
+	/**
+	 * @covers Joomla\Application\Cli\ColorProcessor::replaceColors
+	 */
+	public function testProcessReplace()
+	{
+		$this->assertThat(
+			$this->object->process('<fg=red>foo</fg=red>'),
+			$this->equalTo('[31mfoo[0m')
+		);
+	}
+}
