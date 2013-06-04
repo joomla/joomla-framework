79ca28cfb6075e78d0132191af53ac3decfde0d1
diana <diana.prajescu@gmail.com>
2013-06-04 15:22:05 +0300

    Fix file doc comment.

--- src/Joomla/Twitter/Directmessages.php
+++ src/Joomla/Twitter/Directmessages.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Part of the Joomla Framework Linkedin Package
+ * Part of the Joomla Framework Twitter Package
  *
  * @copyright  Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
