2d86831b3391a5f073caaf21de162bd92848fec2
Asika <asika32764@gmail.com>
2013-11-09 00:41:36 +0800

    Prep for test

---
+++ Tests/ConsoleTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright  Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Console\Tests;
+
+use Joomla\Console\Console;
+use Joomla\Test\TestHelper;
+
+/**
+ * Test class for Console.
+ *
+ * @since  1.0
+ */
+class ConsoleTest extends PHPUnit_Framework_TestCase
+{
+
+}
