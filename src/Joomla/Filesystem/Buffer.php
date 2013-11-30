3a86b843a051c721d2024a3ee12a698a9aa8437f
Michael Babker <mbabker@flbab.com>
2013-11-29 18:18:55 -0600

    Restore stream_open() params to match streamWrapper

--- src/Joomla/Filesystem/Buffer.php
+++ src/Joomla/Filesystem/Buffer.php
@@ -45,14 +45,17 @@ class Buffer
 	/**
 	 * Function to open file or url
 	 *
-	 * @param   string   $path  The URL that was passed
+	 * @param   string   $path          The URL that was passed
+	 * @param   string   $mode          Mode used to open the file @see fopen
+	 * @param   integer  $options       Flags used by the API, may be STREAM_USE_PATH and STREAM_REPORT_ERRORS
+	 * @param   string   &$opened_path  Full path of the resource. Used with STREAN_USE_PATH option
 	 *
 	 * @return  boolean
 	 *
 	 * @since   1.0
 	 * @see     streamWrapper::stream_open
 	 */
-	public function stream_open($path)
+	public function stream_open($path, $mode, $options, &$opened_path)
 	{
 		$url = parse_url($path);
 		$this->name = $url['host'];
