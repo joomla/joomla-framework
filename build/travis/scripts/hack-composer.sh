91e5e18452519a86e4b7a695060922d39f5d46f4
Gary A. Mort <garyamort@gmail.com>
2014-01-11 17:35:56 -0500

    reset aliases on script start

--- build/travis/scripts/hack-composer.sh
+++ build/travis/scripts/hack-composer.sh
@@ -6,19 +6,9 @@ echo "---> Starting $(tput bold ; tput setaf 2)hack of composer$(tput sgr0)"
 git clone git://github.com/garyamort/composer.git $MYCOMPOSER
 cd $MYCOMPOSER
 composer install
-echo "directory"
-ls
-echo "bin directory"
-ls ./bin
-echo "cat composer"
-cat ./bin/composer
-echo "my pwd"
-pwd
 MYCOMPOSEREXEC="$HOME/composer/bin/composer"
 
 echo "---> Creating composer for $(tput bold ; tput setaf 2)$MYCOMPOSEREXEC$(tput sgr0)"
-cat $cat ./bin/composer
-
 echo "Version of local composer"
 $MYCOMPOSEREXEC --version
 alias composer="$MYCOMPOSEREXEC"
