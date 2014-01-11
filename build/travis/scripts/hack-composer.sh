15e44ee1b399fcb86d51f0340af9d837b287eeb2
Gary A. Mort <garyamort@gmail.com>
2014-01-11 17:32:36 -0500

    env var misnamed

--- build/travis/scripts/hack-composer.sh
+++ build/travis/scripts/hack-composer.sh
@@ -15,11 +15,13 @@ cat ./bin/composer
 echo "my pwd"
 pwd
 MYCOMPOSEREXEC="$HOME/composer/bin/composer"
+
 echo "---> Creating composer for $(tput bold ; tput setaf 2)$MYCOMPOSEREXEC$(tput sgr0)"
+cat $cat ./bin/composer
 
 echo "Version of local composer"
-$MYCOMPOSEREXEC$ --version
-alias composer="$MYCOMPOSEREXEC$"
+$MYCOMPOSEREXEC --version
+alias composer="$MYCOMPOSEREXEC"
 echo "Version of composer"
 composer --version
 
