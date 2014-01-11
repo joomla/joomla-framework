eb9193f0e6f7debf8c48e02c93daca1884a535b5
Gary A. Mort <garyamort@gmail.com>
2014-01-11 16:17:33 -0500

    Composer hacks for hhvm nightly

--- build/travis/scripts/hack-composer.sh
+++ build/travis/scripts/hack-composer.sh
@@ -3,7 +3,7 @@ echo on
 MYCOMPOSER="$HOME/composer"
 echo "---> Starting $(tput bold ; tput setaf 2)hack of composer$(tput sgr0)"
 
-git clone git@github.com:garyamort/composer.git $MYCOMPOSER
+git clone git://github.com/garyamort/composer.git $MYCOMPOSER
 cd $MYCOMPOSER
 composer install
 composer update
