c22d9ce78b339a15986ebfa2797959c6ca30a130
Gary A. Mort <garyamort@gmail.com>
2014-01-11 17:02:56 -0500

    Alias composer to local composer moved

--- build/travis/scripts/hack-composer.sh
+++ build/travis/scripts/hack-composer.sh
@@ -7,3 +7,13 @@ git clone git://github.com/garyamort/composer.git $MYCOMPOSER
 cd $MYCOMPOSER
 composer install
 composer update
+
+MYCOMPOSEREXEC="$HOME/composer/bin/composer"
+echo "---> Creating composer for $(tput bold ; tput setaf 2)$MYCOMPOSEREXEC$(tput sgr0)"
+
+echo "Version of local composer"
+$MYCOMPOSEREXEC$ --version
+alias composer="$MYCOMPOSEREXEC$"
+echo "Version of composer"
+composer --version
+
