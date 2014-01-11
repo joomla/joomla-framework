7a9ed09e9392c4f1c5b3c1e00892e0110ec69889
Gary A. Mort <garyamort@gmail.com>
2014-01-11 18:17:53 -0500

    composer mod

--- build/travis/scripts/hack-composer.sh
+++ build/travis/scripts/hack-composer.sh
@@ -6,12 +6,4 @@ echo "---> Starting $(tput bold ; tput setaf 2)hack of composer$(tput sgr0)"
 git clone git://github.com/garyamort/composer.git $MYCOMPOSER
 cd $MYCOMPOSER
 composer install
-MYCOMPOSEREXEC="$HOME/composer/bin/composer"
-
-echo "---> Creating composer for $(tput bold ; tput setaf 2)$MYCOMPOSEREXEC$(tput sgr0)"
-echo "Version of local composer"
-$MYCOMPOSEREXEC --version
-alias composer="$MYCOMPOSEREXEC"
-echo "Version of composer"
-composer --version
 
