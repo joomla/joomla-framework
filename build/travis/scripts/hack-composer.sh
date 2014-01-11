68198f785b93088f9c219ab85bffdbe21a675e2f
Gary A. Mort <garyamort@gmail.com>
2014-01-11 17:20:59 -0500

    path problems on travis

--- build/travis/scripts/hack-composer.sh
+++ build/travis/scripts/hack-composer.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-echo on
+
 MYCOMPOSER="$HOME/composer"
 echo "---> Starting $(tput bold ; tput setaf 2)hack of composer$(tput sgr0)"
 
 git clone git://github.com/garyamort/composer.git $MYCOMPOSER
 cd $MYCOMPOSER
 composer install
-composer update
 
+ls
+ls ./bin
+cat ./bin/composer
+pwd
 MYCOMPOSEREXEC="$HOME/composer/bin/composer"
 echo "---> Creating composer for $(tput bold ; tput setaf 2)$MYCOMPOSEREXEC$(tput sgr0)"
 
