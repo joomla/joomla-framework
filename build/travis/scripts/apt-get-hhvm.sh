8c13a4ffed8ea7e34eb975df646e688b39eccec3
Gary A. Mort <garyamort@gmail.com>
2014-01-11 19:26:46 -0500

    hhvm phpinfo

--- build/travis/scripts/apt-get-hhvm.sh
+++ build/travis/scripts/apt-get-hhvm.sh
@@ -11,4 +11,4 @@ fi
 echo "---> Starting $(tput bold ; tput setaf 2)packets installation$(tput sgr0)"
 echo "---> Packets to install : $(tput bold ; tput setaf 3)$HHVM_PACKETS$(tput sgr0)"
 
-sudo apt-get install -y --force-yes $HHVM_PACKETS"
+sudo apt-get install -y --force-yes $HHVM_PACKETS
