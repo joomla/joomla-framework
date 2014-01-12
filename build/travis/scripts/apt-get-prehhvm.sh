01fd5ea7d926335aeac86dc536cae819952ef32a
Gary A. Mort <garyamort@gmail.com>
2014-01-11 19:10:59 -0500

    hhvm env

--- build/travis/scripts/apt-get-prehhvm.sh
+++ build/travis/scripts/apt-get-prehhvm.sh
@@ -2,7 +2,7 @@
 
 echo $FOO
 
-HHVM_PACKETS="hhvm-nightly"
+HHVM_PACKETS="php5-mcrypt php5-imagick php5-mysql"
 if [ "$1" ]
 then
     HHVM_PACKETS="$EXTRA_PACKETS $1"
@@ -11,4 +11,5 @@ fi
 echo "---> Starting $(tput bold ; tput setaf 2)packets installation$(tput sgr0)"
 echo "---> Packets to install : $(tput bold ; tput setaf 3)$HHVM_PACKETS$(tput sgr0)"
 
+sudo apt-get update
 sudo apt-get install -y --force-yes $HHVM_PACKETS"
