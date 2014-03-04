66d8b9dc7294c067f3ece7c4202368a6e0058fab
aliasm2k <aliasm2k@yahoo.in>
2014-03-04 18:47:08 +0530

    Minor documentation fix

--- src/Joomla/Cache/Tests/WincacheTest.php
+++ src/Joomla/Cache/Tests/WincacheTest.php
@@ -26,7 +26,7 @@ class WincacheTest extends \PHPUnit_Framework_TestCase
 	 *
 	 * @return  void
 	 *
-	 * @coversNothing
+	 * @covers  Nothing
 	 * @since   1.0
 	 */
 	public function testPsrCache()
